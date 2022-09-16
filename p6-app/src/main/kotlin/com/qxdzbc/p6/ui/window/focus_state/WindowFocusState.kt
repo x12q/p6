a1c4c867473ca796493bdcd40f6eb6e158a7f693
qxdzbc <31875822+qxdzbc@users.noreply.github.com>
2022-09-16 21:04:16 +0700

    update: - misc

--- p6-app/src/main/kotlin/com/qxdzbc/p6/ui/window/focus_state/WindowFocusState.kt
+++ p6-app/src/main/kotlin/com/qxdzbc/p6/ui/window/focus_state/WindowFocusState.kt
@@ -3,7 +3,7 @@ package com.qxdzbc.p6.ui.window.focus_state
 import com.qxdzbc.p6.ui.document.worksheet.cursor.state.CursorFocusState
 
 /**
- * Focus state in a window
+ * contain focus state of views in a window
  */
 interface WindowFocusState: CursorFocusState {
     fun freeAllFocus():WindowFocusState
