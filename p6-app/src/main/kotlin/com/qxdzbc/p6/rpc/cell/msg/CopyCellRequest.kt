2c1f199415fd6da7c1b21d7af83e81f5945c1ce1
qxdzbc <31875822+qxdzbc@users.noreply.github.com>
2023-01-31 23:01:12 +0700

    update: - update proto

--- p6-app/src/main/kotlin/com/qxdzbc/p6/rpc/cell/msg/CopyCellRequest.kt
+++ p6-app/src/main/kotlin/com/qxdzbc/p6/rpc/cell/msg/CopyCellRequest.kt
@@ -4,12 +4,15 @@ import com.qxdzbc.p6.proto.CellProtos.CopyCellRequestProto
 import com.qxdzbc.p6.rpc.cell.msg.CellIdDM.Companion.toModel
 
 /**
- * @param shiftRange shift the ranges inside original cell's formula when pasting its content to the new cell or not
+ * A request to copy data and format from [fromCell] to [toCell].
+ * @param shiftRange shift/or not the ranges inside original cell's formula when pasting its content to the new cell.
+ * @param undoable make the action carrying out this request undoable or not.
  */
 class CopyCellRequest(
     val fromCell: CellIdDM,
     val toCell: CellIdDM,
     val shiftRange:Boolean = true,
+    val undoable:Boolean = true,
 ) {
     companion object{
         fun CopyCellRequestProto.toModel(): CopyCellRequest {
@@ -17,6 +20,7 @@ class CopyCellRequest(
                 fromCell = this.fromCell.toModel(),
                 toCell = this.toCell.toModel(),
                 shiftRange = this.shiftRange,
+                undoable = this.undoable,
             )
         }
     }
@@ -25,6 +29,7 @@ class CopyCellRequest(
             .setFromCell(fromCell.toProto())
             .setToCell(toCell.toProto())
             .setShiftRange(shiftRange)
+            .setUndoable(undoable)
             .build()
     }
 }
