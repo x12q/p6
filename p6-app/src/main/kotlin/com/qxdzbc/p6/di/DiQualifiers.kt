ddcce83adfd4026c1b29e4c34f275242409d727b
phong <xadkile@gmail.com>
2023-07-19 22:34:37 +0200

    DI: - refactor

--- p6-app/src/main/kotlin/com/qxdzbc/p6/di/DiQualifiers.kt
+++ p6-app/src/main/kotlin/com/qxdzbc/p6/di/DiQualifiers.kt
@@ -9,65 +9,49 @@ import javax.inject.Qualifier
 annotation class DoNothing
 
 @Qualifier
-@Retention
 annotation class PartialTreeExtractor
 
 @Qualifier
-@Retention
 annotation class PartialCellRangeExtractorQ
 
 @Qualifier
-@Retention
 annotation class TextElementVisitorQ
 
 @Qualifier
-@Retention
 annotation class NullInt
 
 @Qualifier
-@Retention
 annotation class False
 
 @Qualifier
-@Retention
 annotation class FalseMs
 
 @Qualifier
-@Retention
 annotation class True
 
 @Qualifier
-@Retention
 annotation class TrueMs
 
 @Qualifier
-@Retention
 annotation class Fake
 
 @Qualifier
-@Retention
 annotation class Username
 
 @Qualifier
-@Retention
 annotation class AppCoroutineScope
 
 @Qualifier
-@Retention
 annotation class EventServerPort
 
 @Qualifier
-@Retention
 annotation class Null
 
 @Qualifier
-@Retention
 annotation class DefaultTextFieldValue
 
 @Qualifier
-@Retention
 annotation class ActionDispatcherMain
 
 @Qualifier
-@Retention
 annotation class ActionDispatcherDefault
